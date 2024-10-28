e86ac014ba985b9a2977862da9a2d618684787d1
Kamanpreet Singh <kskaman@gmail.com>
2024-10-28 20:21:26 -0230

    updates README.md and updated path specification to login page

--- src/scripts/login-logout.js
+++ src/scripts/login-logout.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const authLink = document.getElementById('auth-link')
     const currentUser = localStorage.getItem('currentUser')
     const lastLogin = localStorage.getItem('lastLogin')
-
+    
+    let loginPagePath = getRootPath() + 'src/pages/login.html'
+    console.log(loginPagePath)    
     if (currentUser && lastLogin) {
         const timeElapsed = Date.now() - parseInt(lastLogin)
         if (timeElapsed < 3600000) {
@@ -16,11 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
             localStorage.removeItem('currentUser')
             localStorage.removeItem('lastLogin')
             authLink.textContent = 'Login/Register'
-            authLink.href = '../pages/login.html'
+            authLink.href = loginPagePath
         } 
     } else {
         authLink.textContent = 'Login/Register'
-        authLink.href = '/workout-logger/src/pages/login.html'
+        authLink.href = loginPagePath
     }
 })
 
@@ -28,5 +30,30 @@ document.addEventListener('DOMContentLoaded', () => {
 function logoutUser() {
     localStorage.removeItem('currentUser')
     localStorage.removeItem('lastLogin')
-    window.location.href = '/workout-logger/src/pages/login.html'
+    let rootPath = getRootPath()
+    window.location.href = rootPath + 'src/pages/login.html'
+}
+
+
+// Function returns the link to login page relative to current page
+function getRootPath() {
+    // Get the path of current page
+    const currentPath = window.location.pathname
+    console.log(currentPath)
+
+
+    // Determine the depth (number of directories) from the root
+    const pathSegments = currentPath.split('/').filter(
+        segment => segment !== '')
+    console.log(pathSegments)
+    // Subtract 1 because the last segment is the current page
+    let depth = pathSegments.length - 1
+
+    // Construct a relative path that goes up to the root
+    let pathToRoot = ''
+    for (let i = 0; i < depth; i++) {
+        pathToRoot += '../'
+    }
+
+    return pathToRoot
 }
